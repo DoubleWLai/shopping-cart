170ebf6022fb9d6d8ad08be2e63a41f173f7430a
lai <doublewlai@gmail.com>
2022-09-02 20:35:26 +0300

    add shopping cart

---
+++ modules/core/src/main/scala/shop/domain/cart.scala
@@ -0,0 +1,22 @@
+package main.scala.shop.domain
+
+import io.estatico.newtype.macros.newtype
+import main.scala.shop.domain.item.{Item, ItemId}
+import squants.market.Money
+
+import java.util.UUID
+
+object cart {
+
+  @newtype case class Quantity(value: Int)
+
+  @newtype case class Cart(items: Map[ItemId, Quantity])
+
+  @newtype case class CartId(value: UUID)
+
+  case class CartItem(item: Item, quantity: Quantity)
+
+  case class CartTotal(items: List[CartItem], total: Money)
+
+
+}
---
+++ modules/core/src/main/scala/shop/services/ShoppingCart.scala
@@ -0,0 +1,18 @@
+package main.scala.shop.services
+
+import main.scala.shop.domain.cart.{Cart, CartTotal, Quantity}
+import main.scala.shop.domain.item.ItemId
+
+trait ShoppingCart[F[_]] {
+
+  def add(userId: UserId, itemId: ItemId, quantity: Quantity): F[Unit]
+
+  def delete(userId: UserId): F[Unit]
+
+  def get(userId: UserId): F[CartTotal]
+
+  def removeItem(userId: UserId, itemId: ItemId): F[Unit]
+
+  def update(userId: UserId, cart: Cart): F[Unit]
+
+}

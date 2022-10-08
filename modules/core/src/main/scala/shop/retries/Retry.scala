0d7fa71c071e4d3afbebb0a179c62b46534feda9
lai <doublewlai@gmail.com>
2022-10-08 15:49:21 +0300

    retry policy

--- modules/core/src/main/scala/shop/retries/Retry.scala
+++ modules/core/src/main/scala/shop/retries/Retry.scala
@@ -1,13 +1,38 @@
 package shop.retries
 
 import cats.effect.Temporal
-
-import scala.concurrent.duration.DurationInt
-
+import cats.syntax.show._
+import org.typelevel.log4cats.Logger
+import retry._
+import retry.RetryDetails.{GivingUp, WillDelayAndRetry}
 
 trait Retry[F[_]] {
 
-  def retry[A](fa: F[A]): F[A] =
-    Temporal[F].sleep(50.milliseconds) >> retry(fa)
+  def retry[A](policy: RetryPolicy[F], retriable: Retriable)(fa: F[A]): F[A]
 
+}
+
+object Retry {
+  def apply[F[_]: Retry]: Retry[F] = implicitly
+
+  implicit def forLoggerTemporal[F[_]: Logger: Temporal]: Retry[F] =
+    new Retry[F] {
+      override def retry[A](policy: RetryPolicy[F], retriable: Retriable)(
+          fa: F[A]
+      ): F[A] = {
+        def onError(e: Throwable, details: RetryDetails): F[Unit] = {
+          details match {
+            case WillDelayAndRetry(_, retriesSoFar, _) =>
+              Logger[F].error(
+                s"Failed on ${retriable.show}. We retried $retriesSoFar times."
+              )
+            case GivingUp(totalRetries, _) =>
+              Logger[F].error(
+                s"Giving up on ${retriable.show} after $totalRetries retries."
+              )
+          }
+        }
+        retryingOnAllErrors[A](policy, onError)(fa)
+      }
+    }
 }

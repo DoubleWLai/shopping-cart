648b2315a1775db636485f61f28c13c0b2582e82
lai <doublewlai@gmail.com>
2022-10-16 10:18:07 +0300

    brandroutes;

--- modules/core/src/main/scala/shop/domain/brand.scala
+++ modules/core/src/main/scala/shop/domain/brand.scala
@@ -1,5 +1,7 @@
 package shop.domain
 
+import derevo.circe.magnolia.{decoder, encoder}
+import derevo.derive
 import io.estatico.newtype.macros.newtype
 
 import java.util.UUID
@@ -8,6 +10,7 @@ object brand {
   @newtype case class BrandId(value: UUID)
   @newtype case class BrandName(value: String)
 
+  @derive(decoder, encoder)
   case class Brand(uuid: BrandId, name: BrandName)
 
 }
---
+++ modules/core/src/main/scala/shop/http/routes/BrandRoutes.scala
@@ -0,0 +1,21 @@
+package shop.http.routes
+
+import cats.Monad
+import org.http4s.HttpRoutes
+import org.http4s.circe.CirceEntityCodec.circeEntityEncoder
+import org.http4s.dsl.Http4sDsl
+import org.http4s.server.Router
+import shop.services.Brands
+
+final case class BrandRoutes[F[_]: Monad](brands: Brands[F])
+    extends Http4sDsl[F] {
+  private[routes] val prefixPath = "/bands"
+
+  private val httpRoutes: HttpRoutes[F] = HttpRoutes.of[F] {
+    case GET -> Root => Ok(brands.findAll)
+  }
+
+  val routes: HttpRoutes[F] = Router(
+    prefixPath -> httpRoutes
+  )
+}
